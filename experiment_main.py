add dt computation; fixed interface to ModelFitter

experiment_main.py
@@ -1,5 +1,6 @@
 from Models import *
 import ModelFitter as mf
+import PidTuner as pt
 import numpy as np
 from csv_rw import read_model_csv
 import matplotlib.pyplot as plt
@@ -14,29 +15,42 @@ import os
 # lin_model_para['D']     = 0
 
 # PID_PARA=dict()
-# PID_PARA['P']=10
-# PID_PARA['I']=5
-# PID_PARA['D']=1
+# PID_PARA['P']=0.
+# PID_PARA['I']=0.
+# PID_PARA['D']=0.
 # lin = LIN_Siso_Model(y_ref=y_ref_step,PID_PARA=PID_PARA,lin_model_para=lin_model_para) 
 # lin.integrate(y0=[0,0,0],t0=0,tf=10,delta_t=0.01)
 
 # Load test data
-t,u,y,r = read_model_csv('D:\\Syno\\PID-Tuner\\ModelTesting\\model_1.csv')
+
+t,u,y,r = read_model_csv('model1.csv')
 
 # fit model
+
 y_lag = 4
-u_lag = 3
+u_lag = 4
 print("Fit model with N = ", len(t) ," ...")
 m_fitter = mf.ModelFitter(u_lag, y_lag) # input lag / output lag
-fitted_model = m_fitter.getFittedModel(u, y)
+m_fitter.fitModel(u[:100], y[:100])
+
+# Forw. sim. of fitted modle using measured inputs:
 
 y_pred = np.zeros(len(y))
 y_pred[:y_lag] = y[:y_lag]
 for i in range(y_lag, len(y-y_lag)):
-	y_pred[i] = fitted_model(u[i-u_lag:i+1], y_pred[i-y_lag:i])
+	y_pred[i] = m_fitter.predict(u[i-u_lag:i+1], y_pred[i-y_lag:i])
+
+# PID fitting:
+
+dt = np.mean(np.diff(t))
+pt = pt.PidTuner(m_fitter, dt)
+
+# Plotting:
 
 plt.plot(t, y)
 plt.plot(t, y_pred)
+plt.legend(['y', 'y_pred'])
+plt.title('u_lag = ' + str(u_lag) + ' y_lag = ' + str(y_lag)) 
 plt.show()
 
 