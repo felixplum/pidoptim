55b9a7d0d7db760fd02cf580425b72b9d4dac447
StfnSky <40562997+StfnSky@users.noreply.github.com>
2018-07-06 14:30:19 +0200

    Add files via upload

--- experiment_main.py
+++ experiment_main.py
@@ -5,7 +5,7 @@ import numpy as np
 from csv_rw import read_model_csv
 import matplotlib.pyplot as plt
 import os
-
+import sys
 # Generate test data, write to file
 # lin_model_para=dict()
 # lin_model_para['name']  = 'model_1'
@@ -23,35 +23,45 @@ import os
 
 # Load test data
 
-t,u,y,r = read_model_csv('model1.csv')
-
+t,u,y,r = read_model_csv('D:\\Syno\\PID-Tuner\\ModelTesting\\test2.CSV')
+y=y-min(y)
+#plt.figure(1)
+#plt.plot(t)
+#plt.figure(2)
+plt.plot(t,y,t,r,t,u)
+#plt.figure(3)
+#plt.plot(t,r)
+plt.show()
 # fit model
-
-y_lag = 4
-u_lag = 4
-print("Fit model with N = ", len(t) ," ...")
-m_fitter = mf.ModelFitter(u_lag, y_lag) # input lag / output lag
-m_fitter.fitModel(u[:100], y[:100])
+#dt=0.1#np.mean(np.diff(t))t_x= t_x[t_x>14200]
+order =7
+#t = np.linspace(0,len(u),dt)
+#print(t)
+#print("Fit model with N = ", len(t) ," ...")
+m_fitter = mf.ModelFitter(order) # input lag / output lag
+m_fitter.fitModel(y, u,t)
 
 # Forw. sim. of fitted modle using measured inputs:
 
-y_pred = np.zeros(len(y))
-y_pred[:y_lag] = y[:y_lag]
-for i in range(y_lag, len(y-y_lag)):
-	y_pred[i] = m_fitter.predict(u[i-u_lag:i+1], y_pred[i-y_lag:i])
+#y_pred = np.zeros(len(y))
+#y_pred[:y_lag] = y[:y_lag]
+#for i in range(y_lag, len(y-y_lag)):
+#	y_pred[i] = m_fitter.predict(u[i-u_lag:i+1], y_pred[i-y_lag:i])
 
 # PID fitting:
 
-dt = np.mean(np.diff(t))
-pt = pt.PidTuner(m_fitter, dt)
+#dt = np.mean(np.diff(t))
+#u_bounds=[-10,10]
+#pid = pt.PidTuner(m_fitter)
+#pid.fitPid(y, r,u,u_bounds,t)
 
 # Plotting:
 
-plt.plot(t, y)
-plt.plot(t, y_pred)
-plt.legend(['y', 'y_pred'])
-plt.title('u_lag = ' + str(u_lag) + ' y_lag = ' + str(y_lag)) 
-plt.show()
+#plt.plot(t, y)
+#plt.plot(t, y_pred)np.sign(pol[i])*pol[i]
+#plt.legend(['y', 'y_pred'])
+#plt.title('u_lag = ' + str(u_lag) + ' y_lag = ' + str(y_lag)) 
+#plt.show()
 
 
 # lin_model_para=dict()
